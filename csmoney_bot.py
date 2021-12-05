36f574b24188a8a398c84fed3c9cc43ec7b35004
pythontoday <pythontodaynet@gmail.com>
2021-12-05 15:03:40 +0700

    Initial commit

---
+++ csmoney_bot.py
@@ -0,0 +1,70 @@
+import json
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+from aiogram.utils.markdown import hbold, hlink
+from main import collect_data
+import os
+import time
+
+bot = Bot(token=os.getenv('TOKEN'), parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    start_buttons = ['🔪 Ножи', '🥊 Перчатки', '🔫 Снайперские винтовки']
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_buttons)
+    
+    await message.answer('Выберите категорию', reply_markup=keyboard)
+    
+
+@dp.message_handler(Text(equals='🔪 Ножи'))
+async def get_discount_knives(message: types.Message):
+    await message.answer('Please waiting...')
+    
+    collect_data(cat_type=2)
+    
+    with open('result.json') as file:
+        data = json.load(file)
+        
+    for index, item in enumerate(data):
+        card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
+            f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
+            f'{hbold("Цена: ")}${item.get("item_price")}🔥'
+    
+    
+        if index%20 == 0:
+            time.sleep(3)
+            
+        await message.answer(card)
+        
+        
+@dp.message_handler(Text(equals='🔫 Снайперские винтовки'))
+async def get_discount_guns(message: types.Message):
+    await message.answer('Please waiting...')
+    
+    collect_data(cat_type=4)
+    
+    with open('result.json') as file:
+        data = json.load(file)
+        
+    for index, item in enumerate(data):
+        card = f'{hlink(item.get("full_name"), item.get("3d"))}\n' \
+            f'{hbold("Скидка: ")}{item.get("overprice")}%\n' \
+            f'{hbold("Цена: ")}${item.get("item_price")}🔥'
+    
+    
+        if index%20 == 0:
+            time.sleep(3)
+            
+        await message.answer(card)
+
+    
+def main():
+    executor.start_polling(dp)
+    
+    
+if __name__ == '__main__':
+    main()
+    
---
+++ main.py
@@ -0,0 +1,73 @@
+from fake_useragent import UserAgent
+import requests
+import json
+
+ua = UserAgent()
+# print(ua.random)
+
+
+def collect_data(cat_type=2):
+    # response = requests.get(
+    #     url='https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=40&hasTradeLock=false&hasTradeLock=true&isStore=true&limit=60&maxPrice=10000&minPrice=1&offset=0&tradeLockDays=1&tradeLockDays=2&tradeLockDays=3&tradeLockDays=4&tradeLockDays=5&tradeLockDays=6&tradeLockDays=7&tradeLockDays=0&type=2&withStack=true',
+    #     headers={'user-agent': f'{ua.random}'}
+    # )
+    
+    # with open('result.json', 'w') as file:
+    #     json.dump(response.json(), file, indent=4, ensure_ascii=False)
+
+    offset = 0
+    batch_size = 60
+    result = []
+    count = 0
+    
+    while True:
+        for item in range(offset, offset + batch_size, 60):
+            # url = item
+            # print(url)
+            
+            url = f'https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=40&hasTradeLock=false&hasTradeLock=true&isStore=true&limit=60&maxPrice=10000&minPrice=2000&offset={item}&tradeLockDays=1&tradeLockDays=2&tradeLockDays=3&tradeLockDays=4&tradeLockDays=5&tradeLockDays=6&tradeLockDays=7&tradeLockDays=0&type={cat_type}&withStack=true'
+            response = requests.get(
+                url=url,
+                headers={'user-agent': f'{ua.random}'}
+            )
+            
+            offset += batch_size
+            
+            data = response.json()
+            items = data.get('items')
+            
+            for i in items:
+                if i.get('overprice') is not None and i.get('overprice') < -10:
+                    item_full_name = i.get('fullName')
+                    item_3d = i.get('3d')
+                    item_price = i.get('price')
+                    item_over_price = i.get('overprice')
+                    
+                    result.append(
+                        {
+                            'full_name': item_full_name,
+                            '3d': item_3d,
+                            'overprice': item_over_price,
+                            'item_price': item_price
+                        }
+                    )
+                    
+        count += 1
+        print(f'Page #{count}')
+        print(url)
+        
+        if len(items) < 60:
+            break
+    
+    with open('result.json', 'w') as file:
+        json.dump(result, file, indent=4, ensure_ascii=False)
+        
+    print(len(result))
+    
+    
+def main():
+    collect_data()
+    
+    
+if __name__ == '__main__':
+    main()
