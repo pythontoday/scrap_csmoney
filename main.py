f043dc6de500e29377653ca6217b2c02d55bad4c
Vladislav <58515947+FrostWolfX@users.noreply.github.com>
2022-09-19 00:26:12 +0300

    Fix offset
    Убрал лишний цикл, который не нужен, т.к. мы и так увеличиваем офсет на 60, его можно использовать передавая в ссылку.

--- main.py
+++ main.py
@@ -14,42 +14,47 @@ def collect_data(cat_type=2):
     count = 0
     
     while True:
-        for item in range(offset, offset + batch_size, 60):
             
-            url = f'https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=40&hasTradeLock=false&hasTradeLock=true&isStore=true&limit=60&maxPrice=10000&minPrice=2000&offset={item}&tradeLockDays=1&tradeLockDays=2&tradeLockDays=3&tradeLockDays=4&tradeLockDays=5&tradeLockDays=6&tradeLockDays=7&tradeLockDays=0&type={cat_type}&withStack=true'
-            response = requests.get(
-                url=url,
-                headers={'user-agent': f'{ua.random}'}
-            )
-            
-            offset += batch_size
-            
-            data = response.json()
-            
-            if data.get('error') == 2:
-                return 'Data were collected'
-            
-            items = data.get('items')
-            
-            for i in items:
-                if i.get('overprice') is not None and i.get('overprice') < -10:
-                    item_full_name = i.get('fullName')
-                    item_3d = i.get('3d')
-                    item_price = i.get('price')
-                    item_over_price = i.get('overprice')
-                    
-                    result.append(
-                        {
-                            'full_name': item_full_name,
-                            '3d': item_3d,
-                            'overprice': item_over_price,
-                            'item_price': item_price
-                        }
-                    )
+        url = f'https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=40&hasTradeLock=false&hasTradeLock=true&isStore=true&limit=60&maxPrice=10000&minPrice=2000&offset={offset}&tradeLockDays=1&tradeLockDays=2&tradeLockDays=3&tradeLockDays=4&tradeLockDays=5&tradeLockDays=6&tradeLockDays=7&tradeLockDays=0&type={cat_type}&withStack=true'
+        response = requests.get(
+            url=url,
+            headers={'user-agent': f'{ua.random}'}
+        )
+        
+        offset += batch_size
+        
+        data = response.json()
+        
+        if data.get('error') == 2:
+            return 'Data were collected'
+        
+        items = data.get('items')
+
+        if items is None:
+            break
+        
+        for i in items:
+            if i.get('overprice') is not None and i.get('overprice') < -10:
+                item_full_name = i.get('fullName')
+                item_3d = i.get('3d')
+                item_price = i.get('price')
+                item_over_price = i.get('overprice')
+                
+                result.append(
+                    {
+                        'full_name': item_full_name,
+                        '3d': item_3d,
+                        'overprice': item_over_price,
+                        'item_price': item_price
+                    }
+                )
                     
         count += 1
         print(f'Page #{count}')
         print(url)
+
+        if len(items) < 60:
+            break
     
     with open('result.json', 'w') as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
